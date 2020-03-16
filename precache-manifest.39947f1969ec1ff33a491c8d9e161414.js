44e1ddb0300c670e004a98cc8734af80644395cc
tiglapiles <18759315970@163.com>
2020-03-16 15:33:22 +0800

    Updates

---
+++ precache-manifest.39947f1969ec1ff33a491c8d9e161414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfdab3129ca264f5023820731f33620",
+    "url": "/simple-video-player-app/index.html"
+  },
+  {
+    "revision": "06a1d3c618b834be5394",
+    "url": "/simple-video-player-app/static/css/2.4495b62d.chunk.css"
+  },
+  {
+    "revision": "19cc1e5d4b4540ede556",
+    "url": "/simple-video-player-app/static/css/main.b1cf29b6.chunk.css"
+  },
+  {
+    "revision": "06a1d3c618b834be5394",
+    "url": "/simple-video-player-app/static/js/2.4e66c6df.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/simple-video-player-app/static/js/2.4e66c6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cc1e5d4b4540ede556",
+    "url": "/simple-video-player-app/static/js/main.6c341506.chunk.js"
+  },
+  {
+    "revision": "1a448eb5b24e699a62b0",
+    "url": "/simple-video-player-app/static/js/runtime-main.d3f26f63.js"
+  }
+]);

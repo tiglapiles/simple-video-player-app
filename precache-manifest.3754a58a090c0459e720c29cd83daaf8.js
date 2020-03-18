3570d9a7726f7a9eaeb1d92ba9d2b3a1a51b8fb4
tiglapiles <18759315970@163.com>
2020-03-18 11:46:05 +0800

    Updates

---
+++ precache-manifest.3754a58a090c0459e720c29cd83daaf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6a7c968daef6b308af0eafe8a26743",
+    "url": "/simple-video-player-app/index.html"
+  },
+  {
+    "revision": "3c45c8d2f6c4d64c60a6",
+    "url": "/simple-video-player-app/static/css/2.4495b62d.chunk.css"
+  },
+  {
+    "revision": "417233c69650f453a10e",
+    "url": "/simple-video-player-app/static/css/main.98e8ec5a.chunk.css"
+  },
+  {
+    "revision": "3c45c8d2f6c4d64c60a6",
+    "url": "/simple-video-player-app/static/js/2.585ac565.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/simple-video-player-app/static/js/2.585ac565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417233c69650f453a10e",
+    "url": "/simple-video-player-app/static/js/main.cd5ea8c6.chunk.js"
+  },
+  {
+    "revision": "1a448eb5b24e699a62b0",
+    "url": "/simple-video-player-app/static/js/runtime-main.d3f26f63.js"
+  },
+  {
+    "revision": "42c024282bc6b4131c58d9f9004ba72e",
+    "url": "/simple-video-player-app/static/media/captions.en.42c02428.vtt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-video-player-app/precache-manifest.530968c11b85518f0de9a12cbcb79d72.js"
+  "/simple-video-player-app/precache-manifest.3754a58a090c0459e720c29cd83daaf8.js"
 );
 
 self.addEventListener('message', (event) => {
